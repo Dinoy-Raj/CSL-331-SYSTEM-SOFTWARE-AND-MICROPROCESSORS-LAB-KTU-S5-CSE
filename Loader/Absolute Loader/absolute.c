13a7698a962215e5cb2c6864ef5c085f6ab3659c
Dinoy-Raj <dinoykraj@gmail.com>
2022-02-06 13:49:26 +0530

    updated absolute loader

--- Loader/Absolute Loader/absolute.c
+++ Loader/Absolute Loader/absolute.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void main()
 {
-    FILE *obj;
-    obj = fopen("objr.txt", "r");
+    FILE *obj, *out;
+    char rec[200];
+    obj = fopen("obj.txt", "r");
+    out = fopen("out.txt", "w");
 
     // if object file is does not exist in specified position to open
     if (obj == NULL)
     {
-        printf("\n The Object File Does not exist in current directory.....\nTerminating ..............");
+        printf("\nThe Object File Does not exist in current directory.....\nTerminating ..............");
         exit(1);
     }
 
     // check each record whether it is text record or header record or end record
+    fscanf(obj, "%s", rec);
+
+    while (rec[0] != "E")
+    {
+    }
+    fscanf(obj, "%s", rec);
+    printf("\n%s", rec);
 }
